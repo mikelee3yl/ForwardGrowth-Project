0690e4dbc2eae69b6ee267609303359e71130b08
mohona17 <mohonaahmed@ufl.edu>
2019-10-31 15:10:35 -0400

    Added about us and blog page, not routed yet

---
+++ client/src/views/About Us/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './About.css';
+
+function About() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+export default About;
---
+++ client/src/views/Blog/Blog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Blog.css';
+
+function Blog() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+export default Blog;
