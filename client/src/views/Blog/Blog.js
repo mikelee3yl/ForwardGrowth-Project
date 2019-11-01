b21a3407f35f2e020fe25df977e5ccfd5adee41c
Michael Lee <mikejlee17@gmail.com>
2019-11-01 14:19:54 -0400

    Embedding twitter take 3

--- client/src/views/Blog/Blog.js
+++ client/src/views/Blog/Blog.js
@@ -6,10 +6,9 @@ function Blog() {
     return (
         <div className="App">
 			<h1>Blog Post Page</h1>
-            <a class="twitter-timeline" href="https://twitter.com/elonmusk?ref_src=twsrc%5Etfw">
-			Tweets by elongatedmuskrat
-			</a>
-			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            componentDidMount: function() {
+				twttr.widgets.load()
+			}
         </div>
     );
 }
