0a67b85b0d1332208ff2d0257b600d19e63ccade
Michael Lee <mikejlee17@gmail.com>
2019-11-01 14:35:15 -0400

    Embedding twitter take 6

--- client/src/views/Blog/Blog.js
+++ client/src/views/Blog/Blog.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 import './Blog.css';
-import twttr from '../../assets/index.html'
+//import twttr from '../../assets/index.html'
 
 function Blog() {
     return (
         <div className="App">
 			<h1>Blog Post Page</h1>
-            componentDidMount: function() {
+        </div>
+		<div>
+		componentDidMount: function() {
 				twttr.widgets.load()
 			}
-        </div>
+		</div>
     );
 }
 
