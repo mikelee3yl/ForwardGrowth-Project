e819400b9fb7951ee3b7826a5e3347503ed144df
Michael Lee <michael.lee@ufl.edu>
2019-11-02 12:45:54 -0400

    Embedding twitter take 14

--- client/src/views/Blog/Blog.js
+++ client/src/views/Blog/Blog.js
@@ -5,6 +5,23 @@ import './Blog.css';
 
 function Blog() {
     return (
+		<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<script>window.twttr = (function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0],
+			t = window.twttr || {};
+		  if (d.getElementById(id)) return t;
+		  js = d.createElement(s);
+		  js.id = id;
+		  js.src = "https://platform.twitter.com/widgets.js";
+		  fjs.parentNode.insertBefore(js, fjs);
+
+		  t._e = [];
+		  t.ready = function(f) {
+			t._e.push(f);
+		  };
+
+			return t;
+		}(document, "script", "twitter-wjs"));</script>
         <div className="App">
 			<h1>Blog Post Page</h1>
             window.addEventListener("load", function() {
