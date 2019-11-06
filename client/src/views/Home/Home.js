b9d8072c9eb0bd4180c435efb82b912fc47ab3f4
marzkemark <marzkemark@ufl.edu>
2019-11-05 19:18:29 -0500

    Changes to Home

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from '../../assets/OrchardGroveLogo.png';
 import './Home.css';
 
+//Home Page: Sofia & Mark
+//Mark: Static
+//Sofia: Boostrap and CSS
+
 function Home() {
     return (
         <div className="App">
@@ -9,6 +13,15 @@ function Home() {
             <a href="/Home"><img src ={logo} width={'50 px'} alt = "logo"></img></a>
             </header>
             <h1>Home Page</h1>
+            <div className="row">
+            <img src="/OrchardGrove(1).png" alt="Backpack Adventures Gaming App"></img>
+            </div>
+            <div className="row">
+            <img src="ForwardGrowthDemo.png" alt="Backpack Adventures Gaming App"></img>
+            </div>
+            <div className="row">
+            <img src="/ForwardGrowthDemo-1.png" alt="Backpack Adventures Gaming App"></img>
+            </div>
         </div>
     );
 }
