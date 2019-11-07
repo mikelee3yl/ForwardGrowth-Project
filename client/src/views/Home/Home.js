129ddc0afee94cc0ea48821ed281e4d7565f5452
marzkemark <marzkemark@ufl.edu>
2019-11-07 13:43:49 -0500

    Added App Store icon (JSX)

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -27,7 +27,7 @@ function Home() {
             <img src={demo1} alt="Backpack Adventures Gaming App"></img>
             </div>
             <div className="row">
-            <a href="https://apps.apple.com/us/app/temple-run/id420009108?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2011-08-04&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"></a>
+            <a href="https://apps.apple.com/us/app/temple-run/id420009108?mt=8" style={{display:inline-block},{overflow:hidden},{background:url('https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2011-08-04&kind=iossoftware&bubble=ios_apps') + no-repeat},{width: '135px'},{height: '40px'}}></a>
             </div>
         </div>
     );
