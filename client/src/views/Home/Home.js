fc1ced4ace829cdc6a1c85691a11cfb81bb7a69f
Sofia Zheng <yunqing.zheng@ufl.edu>
2019-11-07 10:46:50 -0500

    adjusted background

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -14,7 +14,8 @@ function Home() {
         <div className="App">
             <h1>Home Page</h1>       
                 <div className="container-fluid bg-1 text-center">
-                    <h1>Backpack</h1>
+                    <h1>Backpack Adventures</h1>
+                    <button>App Store</button>
                 </div>
         </div>
     );
