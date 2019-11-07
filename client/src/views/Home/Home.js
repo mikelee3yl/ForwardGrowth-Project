e1315e74aedd5f3ed2564d7ebef0025d89454ca9
Sofia Zheng <yunqing.zheng@ufl.edu>
2019-11-07 13:14:22 -0500

    experimenting with container-fluid

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -17,17 +17,17 @@ function Home() {
                     <h1>Backpack Adventures</h1>
                     <button>App Store</button>
                 </div>
-                <div className="container-fluid bg-2 text-center">
-                    {/* features */}
+                <div className="bg-2 text-center">
+                    <h1>Features</h1>
                 </div>
-                <div className="container-fluid bg-3 text-center">
-                    {/* About */}
+                <div className="bg-3 text-center">
+                    <h1>About</h1>
                 </div>
-                <div className="container-fluid bg-4 text-center">
-                    {/* Screenshots */}
+                <div className="bg-4 text-center">
+                    <h1>Screenshots</h1>
                 </div>
-                <div className="container-fluid bg-5 text-center">
-                    {/* Be in touch */}
+                <div className="bg-5 text-center">
+                    <h1>Be in touch</h1>
                 </div>
         </div>
     );
