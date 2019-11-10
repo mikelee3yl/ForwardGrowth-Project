c5cc7fe92a01907911ba824581e47e4765368444
danielluckman <32143321+danielluckman@users.noreply.github.com>
2019-11-10 15:17:37 -0500

    First About commit

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -4,10 +4,62 @@ import './About.css';
 
 function About() {
     return (
-        <div className="App">
-        <h1>About the Team Page</h1>
-    </div>
+        <div>
+            <div className="App">
+                <h1>About the Team Page</h1>
+            </div>
+            {/* <div class="card">
+                <img src="img_avatar.png" alt="Avatar" />
+                <div class="container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect &#38; Engineer</p>
+                </div>
+            </div> */}
+            <div class="card-row">
+                <table>
+                    <tr>
+                        <td>
+                            <div class="flip-card">
+                                <div class="flip-card-inner">
+                                    <div class="flip-card-front">
+                                        <img src="img_avatar.png" alt="Avatar" />
+                                        <div class="container">
+                                            <h4><b>John Doe</b></h4>
+                                            <p>Architect &#38; Engineer</p>
+                                        </div>
+                                    </div>
+                                    <div class="flip-card-back">
+                                        <h1>John Doe</h1>
+                                        <p>Architect &#38; Engineer</p>
+                                        <p>We love that guy</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="flip-card">
+                                <div class="flip-card-inner">
+                                    <div class="flip-card-front">
+                                        <img src="img_avatar.png" alt="Avatar" />
+                                        <div class="container">
+                                            <h4><b>John Doe</b></h4>
+                                            <p>Architect &#38; Engineer</p>
+                                        </div>
+                                    </div>
+                                    <div class="flip-card-back">
+                                        <h1>John Doe</h1>
+                                        <p>Architect &#38; Engineer</p>
+                                        <p>We love that guy</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     );
 }
 
 export default About;
+
