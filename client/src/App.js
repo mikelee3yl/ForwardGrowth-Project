0179efd9bfd403e244798909e46ee439a63c9e6f
mohona17 <mohonaahmed@ufl.edu>
2019-11-16 12:22:41 -0500

    Routed login page and admin dashboard. Added template login and admin pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import About from "./views/About/About.js"
 import Blog from "./views/Blog/Blog.js"
-import NotFound from "./views/NotFound"
+import Login from "./views/Login/Login.js"
 import Header from "./components/Header/Header"
+import AdminDashboard from './views/AdminDashboard/AdminDashboard';
+import NotFound from "./views/NotFound"
 
 
 const App = () => {
@@ -14,11 +16,11 @@ const App = () => {
       
       <Switch>
         <Route exact path="/home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Route exact path="/"><Redirect to="/home" /></Route>
         <Route exact path="/about" component={About}/>
         <Route exact path="/blog" component={Blog}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/admin" component={AdminDashboard}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
---
+++ client/src/views/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+
+    return (
+        <div className="App">
+        <h1>Login Page</h1>
+        </div>
+    );
+}
+
+
+export default Login;
