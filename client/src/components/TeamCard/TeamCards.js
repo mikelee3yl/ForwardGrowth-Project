eba8c6463e4f863f4b5e8cbca7b64e41f367e4cc
mohona17 <mohonaahmed@ufl.edu>
2019-11-18 17:15:18 -0500

    reorganized

--- client/src/components/TeamCard/TeamCards.js
+++ client/src/components/TeamCard/TeamCards.js
@@ -4,19 +4,27 @@ import avatar from '../../assets/img_avatar.png'
 
 //This file is intended to create a grid of cards pulled from the database
 
-class Card extends React.Component {
+class TeamCards extends React.Component {
+
+    showCard = () =>{
+        return (
+            <div class="card">
+                <img src={avatar} alt="Avatar" alt="Avatar" />
+                <div class="container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect &#38; Engineer</p>
+            </div>
+            </div>
+        )
+
+    };
+
     render() {
         return (
-        <div class="card">
-            <img src={avatar} alt="Avatar" alt="Avatar" />
-            <div class="container">
-                <h4><b>John Doe</b></h4>
-                <p>Architect &#38; Engineer</p>
-        </div>
-    </div>
+            this.showCard()
         )
     }
 };
-export default Card;
+export default TeamCards;
 
 
