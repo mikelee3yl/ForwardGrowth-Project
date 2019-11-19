8373f51553b93e3f82eff14608f472e997e40292
Sofia Zheng <yunqing.zheng@ufl.edu>
2019-11-19 12:08:43 -0500

    admin page design rough draft

--- client/src/views/AdminDashboard/AdminDashboard.js
+++ client/src/views/AdminDashboard/AdminDashboard.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './AdminDashboard.css';
+import Collapsible from 'react-collapsible';
 
+const homeTrigger = <h1>Home Page</h1> 
+const blogTrigger = <h1>Blog Page</h1>
 
-function AdminDashboard() {
-
-    return (
+class AdminDashboard extends React.Component{
+ 
+  render() {
+    return(
         <div className="App">
-        <h1>Admin Dashboard</h1>
+          <div className="colStyle">
+      <Collapsible trigger={homeTrigger} className="headerStyle" transitionTime="10" transitionCloseTime="10">
+        <form className ="formStyle" id="homePage">
+            <h3>Features: </h3> 
+            <h4>Company: </h4> <input name="companyText"></input>
+            <h4>Payment Methods: </h4> <input name="paymentMethods"></input>
+            <h3>About: </h3> <input name="aboutText"></input>
+        </form>
+      </Collapsible>
+
+        <Collapsible trigger ={blogTrigger} className="headerStyle" transitionTime="10" transitionCloseTime="10">
+            <form className ="formStyle" id="socialMedia">
+                <h3>Instagram link: </h3> <input name="link"></input>
+            </form>
+        </Collapsible>
+        </div>
         </div>
     );
-}
-
-
+  }
+};
+ 
 export default AdminDashboard;
