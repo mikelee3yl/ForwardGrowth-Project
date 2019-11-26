d25d3057416247f731015d27b91264616a5fea47
marzkemark <marzkemark@ufl.edu>
2019-11-26 15:05:20 -0500

    Changes to Admin Cards

--- client/src/components/TeamCard/TeamCardsAdmin.js
+++ client/src/components/TeamCard/TeamCardsAdmin.js
@@ -63,8 +63,21 @@ render() {
             <div key={index} class="card">
                 <img src={`data:${person.img.contentType};base64,${Buffer.from(person.img.data).toString('base64')}`} alt="" />
                 <div class="container">
-                    <h4><b>{person.name}</b></h4>
-                    <p>{person.position}</p>
+                    <h4>Name: </h4> <textarea type="text" defaultValue={person.name} ref="NewName"></textarea>
+                    <h4>Position: </h4> <textarea type="text" defaultValue={person.position} ref="NewPosition"></textarea>
+                    <h4>Replace photo of team member: </h4>
+                            <input type="file" onChange={this.onChange} />
+                        {/* <button
+                            onClick={() => {
+                                if (person.name) {
+                                    deleteTile(person.name).then(({ message }) => {
+                                        alert(message);
+                                    });
+                                }
+                            }}
+                        >
+                            Update Card
+                    </button> */}
                     <button
                             onClick={() => {
                                 if (person.name) {
