1a765622952df27aa45145bb80cb3bf55ed4951d
mohona17 <mohonaahmed@ufl.edu>
2019-12-01 12:04:52 -0500

    syntax errors

--- client/src/components/TeamCard/TeamCardsAdmin.js
+++ client/src/components/TeamCard/TeamCardsAdmin.js
@@ -69,18 +69,18 @@ class TeamCards extends React.Component {
                                 if(this.state.photo) tileForm.append('file', this.state.photo[0]);
                                 else tileForm.append('file', null);
                                 // console.log("Photo" + this.state.photo[0]); //Object
-                                console.log(tileForm);
+                                console.log(tileForm.values());
 
                                 if (this.refs.NewName.value) tileForm.append('name', this.refs.NewName.value)
                                 else tileForm.append('name', null);
-                                console.log(tileForm);
+                                console.log(tileForm.values());
 
-                                if(this.refs.NewPosition.value) tileForm.append('position'. this.refs.NewPosition.value)
-                                else tileForm.append('position',null)
-                                console.log(tileForm);
+                                if(this.refs.NewPosition.value) tileForm.append('position', this.refs.NewPosition.value)
+                                else tileForm.append('position', null)
+                                console.log(tileForm.values());
 
                                 tileForm.append('originalname', person.name);
-                                console.log(tileForm);
+                                console.log(tileForm.values());
 
                                 updateTile(tileForm).then(({ message }) => {
                                     alert(message);  
