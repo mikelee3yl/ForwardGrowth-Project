4cec8f2c9f8fcc523baa46173e84d2ab32d6b700
marzkemark <marzkemark@ufl.edu>
2019-12-02 11:35:02 -0500

    Automatic re-render

--- client/src/components/TeamCard/TeamCardsAdmin.js
+++ client/src/components/TeamCard/TeamCardsAdmin.js
@@ -103,7 +103,7 @@ class TeamCards extends React.Component {
                                 else {
                                     alert("Cannot update card");                                
                                 }
-                                this.setState(this.state);
+                                //this.setState(this.state);
                             }}
                         >
                             Update Card
@@ -115,7 +115,7 @@ class TeamCards extends React.Component {
                                         alert(message);
                                     });
                                 }
-                                this.setState(this.state);
+                                //this.setState(this.state);
                             }}
                         >
                             Delete Card
