27282c173cf90414a018f964bdedf98965400243
mohona17 <mohonaahmed@ufl.edu>
2019-12-03 10:08:55 -0500

    fixing syntax

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,80 +4,73 @@ import './Header.css';
 import { slide as Menu } from 'react-burger-menu' //https://www.npmjs.com/package/react-burger-menu
 import logo from '../../assets/OrchardGroveLogo.png';
 import menuButton from '../../assets/Hamburger_icon.png';
-import { Redirect } from 'react-router-dom';
+import {Redirect } from 'react-router-dom';
 
 function loginShow() {
-  if (localStorage.getItem('token') !== 'blah') {
-    return <a href="Admin">Admin Dashboard</a>
-  }
-  else {
-    return <a href="Login">Login </a>
-
-  }
+    if (localStorage.getItem('token') !== 'blah') {
+        return <a href="Admin">Admin Dashboard</a>
+    }
+    else {
+        return <a href="Login">Login </a>
+        
+    }
 };
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logo: null,
+const Header = () => {
+    
+  return (
+    // <div class="header" id="myHeader">
+    //   <a href="/Home"><img src={logo} width={'50 px'} align = "center" alt="logo"></img></a>
+    //   </div>
+    <div class="wrapper">
+      <div class="header" align="center">
+              <a href="/Home"><img src={logo} width={'100 px'} alt="logo"></img></a>
+              {(localStorage.getItem('token') !== 'blah' && localStorage.getItem('token') !== null) ?
+                  // <div class="button" align="right">
+                  //     <button>
+                  //         <a href="/Admin">Admin Dashboard</a>
+                  //     </button>
+                  // </div>
+                  < a class="button" alight="right" href="/Admin">Admin Dashboard</a>
 
-    };
-  }
-  componentDidMount() {
-    fetch('/api/get_header')
-      .then(res => {
-        return res.text();
-      })
-      .then(res => {
-        var obj = JSON.parse(res);
-        this.setState({
-          logo: `data:${obj.img.contentType};base64,${Buffer.from(obj.img.data).toString('base64')}`
-        })
-      })
-  }
-  render() {
-    return (
-      // <div class="header" id="myHeader">
-      //   <a href="/Home"><img src={logo} width={'50 px'} align = "center" alt="logo"></img></a>
-      //   </div>
-      <div class="wrapper">
-        <div class="header" align="center">
-          <a href="/Home"><img src={this.state.logo} width={'100 px'} alt="logo"></img></a>
-          {(localStorage.getItem('token') !== 'blah' && localStorage.getItem('token') !== null) ?
-            // <div class="button" align="right">
-            //   <button>
-            //     <a href="/Admin">Admin Dashboard</a>
-            //   </button>
-            // </div>
-
-            < a class="button" alight="right" href="/Admin">Admin Dashboard</a>
-
-            // <div class="button" align="right">
-            //   <button>
-            //     <a href="/Login">Login</a>
-            //   </button>
-            // </div>
-            <a class="button" alight="right" href="/Login">Login</a>
+                  :
+                  // <div class="button" align="right">
+                  //     <button>
+                  //         <a href="/Login">Login</a>
+                  //     </button>
+                  // </div>
+                  < a class="button" alight="right" href="/Login">Login</a>
 
 
-          }
+                  }
 
         </div>
+          
+              
+          
+      <Menu width={'15rem'} customBurgerIcon={<img src={menuButton} />}>
+        <a id="home" className="menu-item" href="/" style={{ textDecoration: 'none' }}>Home</a>
+        <a id="about" className="menu-item" href="/about" style={{ textDecoration: 'none' }}>About the Team</a>
+        <a id="contact" className="menu-item" href="/blog" style={{ textDecoration: 'none' }}>Blog</a>
+
+          </Menu>
+          
+
+          
+    </div>
 
 
 
-        <Menu width={'15rem'} customBurgerIcon={<img src={menuButton} />}>
-          <a id="home" className="menu-item" href="/" style={{ textDecoration: 'none' }}>Home</a>
-          <a id="about" className="menu-item" href="/about" style={{ textDecoration: 'none' }}>About the Team</a>
-          <a id="contact" className="menu-item" href="/blog" style={{ textDecoration: 'none' }}>Blog</a>
 
-        </Menu>
+    //Below is old code for the top navigation default from the MERN Template in case we want to switch back to this
+    // <div className='topnav'>
+    //         <Link className="topnav-link" to="/about">About</Link> */}
 
-
-
-      </div>
-    );
-  };
+    //         <Link className="topnav-link" to='/Home'>Home</Link>
+    //         <Link className="topnav-link" to='/About'>About the Team</Link>
+    //         <Link className="topnav-link" to="/Blog">Blog Posts</Link>
+    //     </div>
+    // </div>
+  )
 }
 
 export default Header;
