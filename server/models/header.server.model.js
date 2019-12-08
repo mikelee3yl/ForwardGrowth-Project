29c0377cae597fce57201ebba3a7c7fbbacc5d2a
mohona17 <mohonaahmed@ufl.edu>
2019-12-07 20:57:57 -0500

    comments for header model

--- server/models/header.server.model.js
+++ server/models/header.server.model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-
+//This file holds the schema for the header image to be stored into the database
 var headerSchema = new Schema({
     img:
         {data:Buffer, contentType:String},
@@ -10,7 +10,6 @@ var headerSchema = new Schema({
 });
 
 headerSchema.pre('save', function (next) {
-
     if (this.img == null)
         throw err;
     var currentDate = new Date();
